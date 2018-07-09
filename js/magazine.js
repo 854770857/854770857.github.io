f3bffc35d91742a7a73158e908e77233bb2865c8
sunny <zhaoxiaogang@citicupb.com>
2018-07-09 16:29:04 +0800

    dd

--- js/magazine.js
+++ js/magazine.js
@@ -63,7 +63,7 @@ function addPage(page, book) {
 function loadPage(page, pageElement) {
     $.ajax({ url: 'pages/page' + page + '.html'}).
     done(function (pageHtml) {
-        pageElement.append(pageHtml)
+        pageElement.append(`${pageHtml}<span class="page-number">${page}</span>`)
     });
 }
 
